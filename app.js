b5605875fda426afa3ccb50c6bc884bc490f31c7
mactuz <malun.058@hotmail.com>
2022-06-26 18:14:49 +0200

    Small change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!<br><br>This is the server speaking...')
+  res.send('Hello World!<br><br>This is the server speaking...<br><br>No extra information needed.')
 })
 
 app.listen(3000, function() {
