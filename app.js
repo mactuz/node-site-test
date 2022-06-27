20e82fad59ff0422fa54d190fc5ad400f57693b9
mactuz <malun.058@hotmail.com>
2022-06-27 15:27:35 +0200

    Basic pug home and account page

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const res = require("express/lib/response");
+require("dotenv").config();
+const app = express();
 
-app.get('/', function (req, res) {
+app.set("view engine", "pug");
+app.use(express.static("public"));
 
-  var time_now= new Date().toTimeString();
- 
-  msg = `Hello World!<br><br>Time now is ${time_now}`
+app.use((req, res, next) => {
+  res.locals.data = { current_time: new Date().toTimeString() };
+  next();
+});
 
-  res.send(msg);
-})
+app.get("/", function (req, res) {
+  res.render("index", {
+    title: "Node Express site",
+    message: "Message for you!",
+  });
+});
+app.get("/account", function (req, res) {
+  res.render("account", {
+    title: "Account settings",
+    message: "Message for you!",
+  });
+});
 
-port = 3000
-
-app.listen(port, function() {
-  console.log('Hello World app listening on port ' + port +'!');
-})
+app.listen(process.env.PORT, function () {
+  msg = "Hello World app listening on port " + process.env.PORT + "!";
+  if (process.env.NODE_ENV == "development") {
+    msg += `\nDev environment: http://localhost:${process.env.PORT}`;
+  } else {
+    msg += `\nProduction environment!`;
+  }
+  console.log(msg);
+});
